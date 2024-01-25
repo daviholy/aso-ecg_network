ff3cbe0607fc68a8897f6e3c147ee1fcb46abab9
FandaCmuchar <fanda.cmuchar@gmail.com>
2024-01-25 20:23:30 +0100

    multi class jaccard and loss tracking with tensorboard

--- src/Model/ecg_model.py
+++ src/Model/ecg_model.py
@@ -2,9 +2,9 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 from torch.optim import AdamW, Optimizer
+from torchmetrics.classification import MulticlassJaccardIndex
 
 from .ecg_network import ECGNetwork
-
 
 class ECGModel(pl.LightningModule):
     def __init__(self, n_classes: int, n_channels: int, learning_rate: float = 1e-4) -> None:
@@ -12,6 +12,7 @@ class ECGModel(pl.LightningModule):
         self.save_hyperparameters()
         self.model = ECGNetwork(n_channels, n_classes)
         self.learning_rate = learning_rate
+        self.jaccard = MulticlassJaccardIndex(num_classes=n_classes)
 
     def configure_optimizers(self) -> Optimizer:
         return AdamW(self.model.parameters(), lr=self.learning_rate)
@@ -20,12 +21,14 @@ class ECGModel(pl.LightningModule):
         signal, annotation = batch
         result = self.model(signal.unsqueeze(1))
         loss = F.cross_entropy(result, annotation)
-        self.log("train_loss", loss)
+        self.log("train_loss", loss, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch: list[torch.Tensor], batch_idx: int) -> torch.Tensor:
         signal, annotation = batch
         result = self.model(signal.unsqueeze(1))
         loss = F.cross_entropy(result, annotation)
-        self.log("val_loss", loss)
+        jaccard_index = self.jaccard(result, annotation)
+        self.log("val_loss", loss, on_epoch=True, prog_bar=True)
+        self.log("val_jaccard_index", jaccard_index, on_epoch=True, prog_bar=True)
         return loss
--- src/__main__.py
+++ src/__main__.py
@@ -2,22 +2,26 @@ import os
 from pathlib import Path
 
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
 
-from .Model.ecg_datamodule import ECGDataModule
-from .Model.ecg_model import ECGModel
+from Model.ecg_datamodule import ECGDataModule
+from Model.ecg_model import ECGModel
 
-ACCELERATOR = "cpu"
+ACCELERATOR = "gpu"
 
 # TODO: add config file
 # TODO: add data augmentation
 # TODO: add logger (tensorboard or aim?)
 # TODO: add checkpointing and result model save
+def load_last_checkpoint(checkpoint_dir: Path):
+    pass
+
 
 if __name__ == "__main__":
     ecg_dir = Path("data")
     n_channels = 1
-    n_classes = 3
-    batch_size = 32
+    n_classes = 4 # - nothing, P, QRS, T
+    batch_size = 64
     learning_rate = 1e-4
     num_of_workers = os.cpu_count()
 
@@ -26,6 +30,8 @@ if __name__ == "__main__":
         ecg_dir=ecg_dir, num_of_workers=num_of_workers, batch_size=batch_size, train_ratio=0.8
     )
 
+    tensor_board_logger = TensorBoardLogger('./logs', name='ecg_p_qrs_t')
+
     model = ECGModel(n_channels=n_channels, n_classes=n_classes, learning_rate=learning_rate)
-    trainer = pl.Trainer(max_epochs=10, accelerator=ACCELERATOR)
+    trainer = pl.Trainer(max_epochs=300, accelerator=ACCELERATOR, logger=tensor_board_logger)
     trainer.fit(model, ecg_data_module)

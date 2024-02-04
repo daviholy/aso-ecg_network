0a175c3c5bf5bab24db7356c81796e75bf41ad2c
FandaCmuchar <fanda.cmuchar@gmail.com>
2024-02-04 21:53:00 +0100

    add signal cropping with no annotation

--- src/common/data_classes.py
+++ src/common/data_classes.py
@@ -14,3 +14,11 @@ class AnnotationSymbols:
     p: list[Range]
     N: list[Range]
     t: list[Range]
+
+    def get_min_start_idx(self) -> int:
+        res = [s[0].start for s in [self.p, self.N, self.t] if len(s) > 0]        
+        return min(res)
+    
+    def get_max_end_idx(self) -> int:
+        res = [s[-1].end for s in [self.p, self.N, self.t] if len(s) > 0]        
+        return max(res)

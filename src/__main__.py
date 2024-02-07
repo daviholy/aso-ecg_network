b05fba76e94774bf3fddde3a57d82a6fb498a589
FandaCmuchar <fanda.cmuchar@gmail.com>
2024-02-07 18:39:37 +0100

    ploting model results

--- src/__main__.py
+++ src/__main__.py
@@ -1,93 +1,71 @@
 import os
 from pathlib import Path
+import torch
+from wfdb.processing import resample_sig
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 
 from model.ecg_datamodule import ECGDataModule
 from model.ecg_model import ECGModel
+from model.ecg_model_inference import ModelInference
 
-import plotly.graph_objects as go
-import numpy as np
+from common.config import load_config
+from common.utils import plot_signal_with_labels_and_predictions
 
-ACCELERATOR = "gpu"
+import pydicom
 
 # TODO: add config file
 # TODO: make separate file for visualization?
 # TODO: make test pipeline in datamodule in TensorBoard 
 # TODO: in all data cut start and end signal values because it is not annotated
 
-def plot_signal_with_labels(signal, labels):
-    colors = {0: 'blue', 1: 'red', 2: 'green', 3: 'violet'}
-    fig = go.Figure()
-    start_idx = 0
-
-    fig.add_trace(go.Scatter(
-    x=np.arange(len(signal)),
-    y=signal,
-    mode='lines',
-    line=dict(color='black'),
-    showlegend=False))
-
-    for idx in range(1, len(labels)):
-        if labels[idx] != labels[idx - 1]:
-            fig.add_trace(go.Scatter(
-                x=np.arange(start_idx, idx), 
-                y=signal[start_idx:idx], 
-                mode='lines+markers',
-                line=dict(color=colors[labels[idx-1].item()]),
-                showlegend=False))
-            start_idx = idx
-
-    fig.add_trace(go.Scatter(
-        x=np.arange(start_idx, len(labels)), 
-        y=signal[start_idx:len(labels)], 
-        mode='lines+markers',
-        line=dict(color=colors[labels[-1].item()]),
-        showlegend=False))
-
-    
-    fig.update_layout(title='Signal Values Colored by Class',
-                xaxis_title='X Axis',
-                yaxis_title='Signal Value',
-                legend_title='Legend')
-
-    fig.show()
-
-
 if __name__ == "__main__":
-    ##################################
-    train_model = True
-    ##################################
-    ecg_dir = Path("data")
-    n_channels = 1
-    n_classes = 4 # - nothing, P, QRS, T
-    batch_size = 64
-    learning_rate = 4e-4
-    num_of_workers = os.cpu_count()
-    resample_fs = 100
+    config = load_config("./src/config.yaml")
 
     # Initialize ECG Data Module
     ecg_data_module = ECGDataModule(
-        ecg_dir=ecg_dir, num_of_workers=num_of_workers, batch_size=batch_size, train_ratio=0.8, resample_fs=resample_fs
+        ecg_dir=config.dataloader.ecg_dir, 
+        num_of_workers=config.dataloader.num_workers,
+        batch_size=config.dataloader.batch_size, 
+        train_ratio=config.dataloader.train_ratio, 
+        resample_fs=config.dataloader.resample_fs
     )
 
-    tensor_board_logger = TensorBoardLogger('./logs', name='ecg_p_qrs_t')
+    tensor_board_logger = TensorBoardLogger('./logs', name='ecg_p_qrs_t') if config.model.train else None
 
     
-    if train_model:
-        model = ECGModel(n_channels=n_channels, n_classes=n_classes, learning_rate=learning_rate)
-        trainer = pl.Trainer(max_epochs=200, accelerator=ACCELERATOR, logger=tensor_board_logger)
+    trainer = pl.Trainer(max_epochs=config.training.max_epochs, 
+                            accelerator=config.training.accelerator, 
+                            logger=tensor_board_logger)
+    
+    if config.model.train:
+        model = ECGModel(n_channels=config.model.n_channels, 
+                         n_classes=config.model.n_classes, 
+                         learning_rate=config.model.learning_rate)
         trainer.fit(model, ecg_data_module)
     else:
-        # FIXME: separate file
-        model = ECGModel.load_from_checkpoint(Path("./logs/ecg_p_qrs_t/best_model/checkpoints/epoch=199-step=6000.ckpt"))
-        model.to("cpu")
-        ecg_data_module.setup()
-        signal, target = ecg_data_module.test[1]
-        pred = model.predict(signal.unsqueeze(0).unsqueeze(0)).squeeze(0)
-        pred = pred.argmax(0)
+        model_inference = ModelInference(Path("./logs/ecg_p_qrs_t/best_model/checkpoints/epoch=199-step=6000.ckpt"))
+        
+        signal_path = Path('./data/our_data/david.dcm')
+        dicom_data = pydicom.dcmread(signal_path)
+        signals = dicom_data.waveform_array(0)
+        sampling_freq = int(dicom_data.WaveformSequence[0].SamplingFrequency)
 
-        plot_signal_with_labels(signal, target)
-        plot_signal_with_labels(signal, pred)     
+        resampled_signal = resample_sig(signals[:, 1], sampling_freq, config.dataloader.resample_fs)[0]
+        sig_len_10_min = int(config.dataloader.resample_fs * 600)
+        
+        input_signal = resampled_signal[-sig_len_10_min:]
+        input_signal = input_signal[: - config.dataloader.resample_fs * 10]
+
+        full_pred = model_inference.process_signal(input_signal, config.dataloader.resample_fs)
+        plot_signal_with_labels_and_predictions(input_signal, full_pred, save_path="./statistics", name=signal_path.stem)
+
+        trainer.test(model_inference.model, ecg_data_module)
+
+        signal, target = ecg_data_module.test[1+6]
+
+        pred = model_inference.perform_inference(signal)
+        model_inference.display_prediction(signal=signal, pred=pred, target=target)
+
 
